ed5b6e3eacfb26f3d561512c2eabc082f4dd149f
fijiwater24 <voxel0@disroot.org>
2025-10-14 16:07:47 -0400

    testing for now

---
+++ js/cloaker.js
@@ -0,0 +1,38 @@
+document.addEventListener("keydown", function (event) {
+    if (event.key === ".") {
+        let choice = prompt("CLOAK OPTIONS: ALEKS (a), Clever (c), Cool Math Games (cl), Desmos (d), Edpuzzle (e), Google (gl), Google Drive (g), Google Classroom (gc), Google Docs (gd), Google Slides (gs), IXL (i), i-Ready Reading (ir), i-Ready Math (im), Khan Academy (k), Naviance (n), PowerSchool (p), Schoology (s)");
+
+        const options = {
+            g: { title: "My Drive - Google Drive", icon: "/img/cloaker/drive.ico" },
+            gc: { title: "Google Classroom", icon: "/img//cloaker/classroom.ico" },
+            c: { title: "Clever | Portal", icon: "/img/cloaker/clever.ico" },
+            p: { title: "Student and Parent Sign In", icon: "/img/cloaker/powerschool.ico" },
+            k: { title: "Khan Academy", icon: "/img/cloaker/khan.ico" },
+            gd: { title: "Google Docs", icon: "/img/cloaker/docs.ico" },
+            s: { title: "Schoology", icon: "/img/cloaker/schoology.ico" },
+            d: { title: "Desmos | Scientific Calculator", icon: "/img/cloaker/desmos.ico" },
+            cl: { title: "Cool Math Games - Free Online Games for Learning and Fun", icon: "/img/cloaker/coolmathgames.ico" },
+            i: { title: "IXL | Math, Language Arts, Science, Social Studies, and Spanish", icon: "/img/cloaker/ixl.ico" },
+            im: { title: "Math To Do, i-Ready", icon: "/img/cloaker/iready.ico" },
+            ir: { title: "Reading To Do, i-Ready", icon: "/img/cloaker/iready.ico" },
+            gl: { title: "google classroom - Google Search", icon: "/img/cloaker/google.ico" },
+            gs: { title: "Google Slides", icon: "/img/cloaker/slides.ico" },
+            a: { title: "ALEKS - Home", icon: "/img/cloaker/aleks.ico" },
+            e: { title: "Edpuzzle", icon: "/img/cloaker/edpuzzle.ico" },
+            n: { title: "Naviance Student", icon: "/img/cloaker/naviance.ico" },
+        };
+
+        if (options[choice]) {
+            document.title = options[choice].title;
+            changeFavicon(options[choice].icon);
+        }
+    }
+});
+
+function changeFavicon(src) {
+    let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = "image/x-icon";
+    link.rel = "shortcut icon";
+    link.href = src;
+    document.getElementsByTagName("head")[0].appendChild(link);
+}
